1b2f65fcccc25e48eb958629163d133184386047
Silviu Schiau <pr@silviu.co>
2014-12-18 20:27:23 -0500

    Bug fix

--- Particle.js
+++ Particle.js
@@ -1,50 +1,86 @@
-/*
-
-	Author: Silviu Schiau (@sschiau on Twitter)
-	Web: www.schiau.co
-	File: Particle.js
-	Created: 1393394887905 (UNIX Time)
-	Modified: 1393394887905 (UNIX Time)
-	About: JavaScript implementation of Twitter Snowflake ID Generator (Extended to 42bits epoch for 96 years 1 month 21 days 16 hours 42 minutes 24 seconds in the future)
-	License: Apache License Version 2.0 http://www.apache.org/licenses/LICENSE-2.0.txt
-
-	Thanks to Twitter for Snowflake.
-	This header should NOT be removed if you want to use Particle.
-    
-*/
+/**
+ * Copyright 2014 Silviu Schiau.
+ *
+ * This copyright notice
+ * shall be included in all copies or substantial portions of the software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * @about JavaScript implementation of Twitter Snowflake ID Generator (64 bit ID)
+ * @author Silviu Schiau <pr@silviu.co>
+ * @package Schiau
+ * @created 1393394887 (UNIX Time)
+ * @modified 1418950507 (UNIX Time)
+ * @version 1.0.1
+ * @license Apache License Version 2.0 http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ * Thanks to Twitter for Snowflake.
+ */
 
 function Particle()
 {
-    const epoch = 1393394887905;
+    const epoch = 1418951572546;
     
     this.generateParticle = function(machine_id)
     {
-        // Time - 42 bits (millisecond precision w/ a custom epoch gives us 96 years 1 month 21 days 16 hours 42 minutes 24 seconds in the future)
+		/* 
+		* Time - 41 bits
+		*/
         time = Math.floor(new Date());
-        
-        // Substract custom epoch from current time
+
+		/*
+		* Substract custom epoch from current time
+		*/
         time -= epoch;
         
-        // Add to base
-        base = Math.pow(2,41);
-        base += time;
-        base = base.toString(2);
-        
-        // Configured machine id - 10 bits - to 1024 machines
-        machine_id = machine_id.toString(2);
-        
-        // Sequence number - 12 bits - up to 4096 random numbers per machine
-        random = Math.floor(Math.random() * (Math.pow(2,12)-1));
-        random = random.toString(2);
-        
-        // Pack
-        base = base + machine_id + random;
-        
-        return parseInt(base, 2);
-    }
+		/*
+		* Create a base and add time to it
+		*/
+		base = (Math.pow(2,40) - 1 + time).toString(2);
+		
+		/*
+		* Configured machine id - 10 bits - up to 512 machines
+		*/
+		machine_id = (Math.pow(2,9) - 1 + machine_id).toString(2);
+		
+		/*
+		* sequence number - 12 bits - up to 2048 random numbers per machine
+		*/
+		random = generateRandomInt(1, (Math.pow(2,11) - 1))
+		random = (Math.pow(2,11) - 1 + random).toString(2);
 
+		/*
+		* Pack
+		*/
+		base = base + machine_id + random;
+		
+		/*
+		* Return unique time id no
+		*/
+		return parseInt(base, 2);
+    }
+	
+	
     this.timeFromParticle = function(particle)
     {
-        return parseInt(particle.toString(2).substring(0,42), 2) - Math.pow(2,41) + epoch;
+		/*
+		* Return time
+		*/
+		return parseInt(particle.toString(2).substring(0,41), 2) - Math.pow(2,40) - 1 + epoch;
     }
+	
+	/*
+	* Generate random Int in interval
+	*/
+	function generateRandomInt(min, max) 
+	{
+	  return Math.floor(Math.random() * (max - min)) + min;
+	}
+	
 }
--- README.md
+++ README.md
@@ -1,24 +1,23 @@
-#Particle 
-###64bits int Time Based ID Generator
-JavaScript extended implementation of Twitter Snowflake ID Generator (Extended to 42bits epoch for 96 years 1 month 21 days 16 hours 42 minutes 24 seconds in the future)
+#Particle
+####Language: JavaScript 
+####64bits int Time Based ID Generator
+JavaScript implementation of Twitter Snowflake ID Generator
 
-
-## Uncoordinated
+### Uncoordinated
 For high availability within and across data centers, machines generating ids should not have to coordinate with each other.
 
-##  Solution
+### Solution
 * id (64 bits) is composed of:
-  * time - 42 bits (millisecond precision w/ a custom epoch gives us 96 years 1 month 21 days 16 hours 42 minutes 24 seconds in the future)
-  * configured machine id - 10 bits - up to 1024 machines
-  * sequence number - 12 bits - up to 4096 random numbers
+  * time - 41 bits (millisecond precision w/ a custom epoch)
+  * configured machine id - 10 bits - up to 512 machines
+  * sequence number - 12 bits - up to 2048 random numbers
 
 ### System Clock Dependency
 You should use NTP to keep your system clock accurate.
 
 ## How to use it
-### Generate Particle ID
-### Time from Particle ID (w/ milisecond precision)
-Check Demo.html gile
+Check Demo.html
+
 
 ## License
 Apache License Version 2.0
